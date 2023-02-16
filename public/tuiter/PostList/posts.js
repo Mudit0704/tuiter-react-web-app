0ab076a767a860964fdfb829e1be951b176fa721
Mudit0704 <maheshwari7mudit@gmail.com>
2023-02-16 18:28:21 -0500

    A5 completed.

--- public/tuiter/PostList/posts.js
+++ public/tuiter/PostList/posts.js
@@ -1,23 +1,23 @@
 export default [
   {
-    avatarIcon: '../../images/react.jpg',
+    avatarIcon: '../../images/insp4.png',
     userName: 'Elon Musk',
     handle: 'elonmusk',
     time: '23h',
     title: 'Amazing show about @Inspiration4x mission!',
-    image: '../../images/starship.jpg',
+    image: '../../images/insp4_content.jpg',
     imageTitle: 'Countdown: Inspiration4 Mission to Space | Netflix Official Site',
     imageContent: 'From training to launch to landing, this all-access docuseries rides along with the Inspiration4 crew on the first all-civilian orbital space ...',
     commentCount: '4.2K',
     retweetCount: '3.5K',
     likeCount: '37.5K'
   },{
-    avatarIcon: '../../images/java.jpg',
+    avatarIcon: '../../images/nypost.png',
     userName: 'New York Post',
     handle: 'nypost',
     time: '23h',
     title: 'Grimes seen reading Karl Marx following split with world\'s richest man Elon Musk trib.al/nx2Gfaq',
-    image: '../../images/starship.jpg',
+    image: '../../images/nypost_content.jpg',
     imageTitle: '',
     imageContent: '',
     commentCount: '965',
